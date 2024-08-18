e014c74192c78b263493f6d8430a88c61ae795ff
Konstantin Kushnir <chpock@gmail.com>
2024-08-18 20:35:51 +0000

    Fix typo

--- https.tcl
+++ https.tcl
@@ -3,7 +3,7 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 
-package require https
+package require http
 
 if { ![catch { package require mtls }] } {
 
